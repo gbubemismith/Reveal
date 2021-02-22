a157f4368d701d07e70939e3f2f91c33e2658914
gbubemismith <gsmithwalter@gmail.com>
2021-02-23 00:56:10 +0100

    Added generic attributes class

--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -1,11 +1,20 @@
 export class Attributes<T> {
   constructor(private data: T) {}
 
-  get(propName: string): number | string | boolean {
-    return this.data[propName];
+  //K can only be one of the keys of T
+  get<K extends keyof T>(key: K): T[K] {
+    return this.data[key];
   }
 
   set(updateData: T): void {
     Object.assign(this.data, updateData);
   }
 }
+
+// const attr = new Attributes<UserProps>({
+//   id: 5,
+//   bio: 'test',
+//   name: 'smnith',
+// });
+
+// const name = attr.get('id');
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,7 @@
 import { Sync } from './Sync';
 import { Eventing } from './Eventing';
 import axios, { AxiosResponse } from 'axios';
+import { Attributes } from './Attributes';
 
 export interface UserProps {
   id?: number;
@@ -13,4 +14,9 @@ const rootUrl = 'http://localhost:3000/users';
 export class User {
   public events: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+  public attributes: Attributes<UserProps>;
+
+  constructor(public attrs: UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs);
+  }
 }
